3b4da77111c75d3edac56e3220cf3715489f4d1a
donox <don@theoxleys.co>
2024-05-15 09:30:04 -0400

    Initial backup

---
+++ assistant/grant_writer.py
@@ -0,0 +1,105 @@
+from openai import OpenAI
+from openai import AssistantEventHandler
+from typing_extensions import override
+
+assistant_instructions = """Attempt to fill out the Letter of Intent (LOI) using information from the vector store 
+wherever possibly.  Where you are unable to determine an appropriate response, insert a note enclosed in angle brackets 
+indicating that you could not develop the response and, if possible, why not.  Do not guess or make up facts not in 
+evidence."""
+
+
+
+class GrantWriter(object):
+    def __init__(self):
+        self.client = OpenAI(api_key="sk-proj-Ng3VygmToAf4sehVA5k8T3BlbkFJfVrRFXj0pESrGEDDbM2Z")
+        self.vector_stores = []
+        self.assistant = self.client.beta.assistants.create(
+            name="Grant Writer",
+            instructions=assistant_instructions,
+            tools=[{"type": "file_search"}],
+            tool_resources={"file_search": {"vector_store_ids": []}},
+            model="gpt-4o",
+        )
+        self.thread = self.client.beta.threads.create()
+        self.test_file = self.client.files.create(file=open("/home/don/Documents/Wonders/test forms/Kronkosky - LOI.docx", "rb"),
+                                        purpose="assistants")
+        self.message = self.client.beta.threads.messages.create(
+            thread_id=self.thread.id,
+            role="user",
+            content="Fill out the LOI in the associated file using information from the vector store",
+            attachments=[{"file_id": self.test_file.id,
+                          "tools": [{"type": "file_search"}]}]
+        )
+        # self.message2 = self.client.beta.threads.messages.create(
+        #     thread_id=self.thread.id,
+        #     role="user",
+        #     content="What are the expenses for Wonders and Worries in 2023?  The necessary data is in the vector store."
+        # )
+
+    def run_assistant(self):
+        # Then, we use the `stream` SDK helper
+        # with the `EventHandler` class to create the Run
+        # and stream the response.
+        # print(f"AT RUN - Assistant ID: {self.assistant.id}")
+        with self.client.beta.threads.runs.stream(
+                thread_id=self.thread.id,
+                assistant_id=self.assistant.id,
+                # instructions="Display the answers with the numbers written out",
+                event_handler=EventHandler(),
+        ) as stream:
+            stream.until_done()
+
+    def add_vector_store(self, vector_store_id):
+        self.assistant = self.client.beta.assistants.update(
+            assistant_id=self.assistant.id,
+            tool_resources={"file_search": {"vector_store_ids": [vector_store_id]}}
+        )
+        # self.vector_stores.append(vector_store_id)
+        # print(f"AT ADD - Assistant ID: {self.assistant.id}")
+        # print(f"VS: {vector_store_id}, {self.vector_stores}")
+
+    def get_client(self):
+        return self.client
+
+
+class EventHandler(AssistantEventHandler):
+    """    # First, we create a EventHandler class to define
+    # how we want to handle the events in the response stream."""
+
+    @override
+    def on_text_created(self, text) -> None:
+        print(f"\nassistant > ", end="", flush=True)
+
+    @override
+    def on_text_delta(self, delta, snapshot):
+        print(delta.value, end="", flush=True)
+
+    def on_tool_call_created(self, tool_call):
+        print(f"\nassistant > {tool_call.type}\n", flush=True)
+
+    def on_tool_call_delta(self, delta, snapshot):
+        if delta.type == 'code_interpreter':
+            if delta.code_interpreter.input:
+                print(delta.code_interpreter.input, end="", flush=True)
+            if delta.code_interpreter.outputs:
+                print(f"\n\noutput >", flush=True)
+                for output in delta.code_interpreter.outputs:
+                    if output.type == "logs":
+                        print(f"\n{output.logs}", flush=True)
+
+
+class MakeVectorStore(object):
+    def __init__(self, client, name):
+        self.client = client
+        self.store_name = name
+        self.vector_store = client.beta.vector_stores.create(name=name)
+
+    def make_store(self, file_list):
+        file_streams = [open(path, "rb") for path in file_list]
+
+        # Use the upload and poll SDK helper to upload the files, add them to the vector store,
+        # and poll the status of the file batch for completion.
+        file_batch = self.client.beta.vector_stores.file_batches.upload_and_poll(
+            vector_store_id=self.vector_store.id, files=file_streams
+        )
+        return file_batch
---
+++ driver.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import configparser
+import datetime as dt
+import os
+# import shutil
+import traceback
+from pathlib import Path
+from utilities.run_log_command import BasicLogger, list_files
+from assistant.grant_writer import GrantWriter, MakeVectorStore
+
+# from external_sites.manage_google_drive import ManageGoogleDrive
+
+
+
+# RClone config file in /home/don/.config/rclone/rclone.conf
+
+def driver():
+    # This script runs daily
+    do_testing = True
+
+    start_notest = dt.time(1, 0)     # but not if between 1am and 4am
+    end_notest = dt.time(4, 0)
+    if start_notest < dt.datetime.now().time() < end_notest:
+        do_testing = False
+
+    if do_testing:
+        prototyping = False
+        write_grants = True
+    else:
+        prototyping = False
+        write_grants = False
+
+    pth = os.path.abspath(os.curdir)            # Find current directory (where we are running)
+
+    config = configparser.ConfigParser()
+
+    with open("/home/don/PycharmProjects/grant_assistant/config_file.cfg") as source:
+        config.read(source.name)
+    # Load parameters from configuration file
+    start_date = config['measurement period']['startDate']
+    end_date = config['measurement period']['endDate']
+
+    start_measurement = (dt.datetime.strptime(start_date, "%Y/%m/%d")).date()
+    end_measurement = (dt.datetime.strptime(end_date, "%Y/%m/%d")).date()
+
+    dbname = config['database']['dbName']
+    dbuser = config['database']['dbUser']
+
+    os.curdir = config['paths']['workingDirectory']         # Set current working directory
+    work_directory = config['paths']['workingDirectory']
+    logs_directory = config['paths']['logsDirectory']
+
+    don_dir = Path('/home/don')                             # Don's home directory
+    don_devel = don_dir / 'devel'                           # Development Directory (the '/' is a path join operator)
+
+    # Linix commands to access Google Drive
+    cmd_rclone = 'rclone -v copyto {} gdriveremote:/RClone/{}'
+    # cmd_save_sst_files = "rclone -v copyto {} 'gdriveremote:/Sunnyside Times/SST Admin/{}'"
+    # cmd_get_sst_files = "rclone -v copy 'gdriveremote:/Sunnyside Times/SST Admin/{}' {}"
+
+    summary_logger = BasicLogger('summary_log', logs_directory)     # Logger - see use below
+    summary_logger.make_info_entry('Start Summary Log')
+
+    if write_grants:
+        logger = BasicLogger('prototyping', logs_directory)
+        target_directory = work_directory + 'worktemp/'
+        try:
+            vector_store_id = config['vectorstore']['id']
+            grant_builder = GrantWriter()
+            grant_builder.add_vector_store(vector_store_id)
+            grant_builder.run_assistant()
+
+            # store = MakeVectorStore(grant_builder.get_client(), "VStore")
+            # file_list = list_files("/home/don/PycharmProjects/grant_assistant/body_of_knowledge")
+            # res = store.make_store(file_list)
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+        logger.close_logger()
+
+    if prototyping:
+        logger = BasicLogger('prototyping', logs_directory)
+        target_directory = work_directory + 'worktemp/'
+        try:
+            from practice_one import DoNothingWell
+            message1 = None
+            message2 = 37
+            my_first_object = DoNothingWell(message1, message2)   # Do interesting stuff here
+            my_first_object.print_something("A Message of Interest")
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+        logger.close_logger()
+
+
+    summary_logger.make_info_entry('Summary Log Entries Completed')
+    summary_logger.close_logger()
+
+
+if __name__ == '__main__':
+    driver()
